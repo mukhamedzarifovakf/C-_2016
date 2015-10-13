9ccc26f0e08f57833f114cd8ffec2a2e5aae3652
mukhamedzarifovakf <mukhamedzarifovakf@gmail.com>
2015-10-13 11:42:29 +0300

    7.3

--- Prac7/7.3.py
+++ Prac7/7.3.py
@@ -1,7 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 def y(x):
-    y = np.log((x ** 2 + 1) * np.exp(-abs(x) / 10), 1 + 1 / np.tan(1/(1 + np.sin(x) ** 2)))
-x = np.arange(10, 10.01, 0.1)
+    y = np.log((x ** 2 + 1) * np.exp(-np.abs(x) / 10), 1 + 1 / np.tan(1/(1 + np.sin(x) ** 2)))
+    return y
+x = np.arange(-10, 10.01, 0.1)
 plt.plot(x, y(x), lw = 3)
 plt.show()
