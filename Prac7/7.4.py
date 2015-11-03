fc4cf7de4c74d86710ec8b28ecadfb3fbe460c08
mukhamedzarifovakf <mukhamedzarifovakf@gmail.com>
2015-11-03 09:28:23 +0300

    Some files

---
+++ Prac7/7.4.py
@@ -0,0 +1,12 @@
+import math
+import pylab
+import matplotlib as plt
+
+xmin = -20
+xmax = 20
+dx = 0.2
+ymin = -20
+ymax = 20
+dy = 0.2
+xlist = plt.mlab.frange(xmin, xmax, dx)
+xlist = plt.mlab.frange(ymin, ymax, dy)
