28b18e255abb3d595dcee680459c2e19ca4007c9
mukhamedzarifovakf <mukhamedzarifovakf@gmail.com>
2015-11-16 22:19:58 +0300

    Prac11

--- Prac11/CW11.5.py
+++ Prac11/CW11.5.py
@@ -1,4 +1,3 @@
-import math
 class Point:
     x = 0
     y = 0
@@ -12,26 +11,32 @@ class Point:
     def __truediv__(self, number):
         return Point(str(self.x / number) + ',' + str(self.y / number))
     def Distance(self):
-        return math.sqrt(self.x ** 2 + self.y ** 2)
+        return (self.x ** 2 + self.y ** 2) ** 0.5
     def DistanceTo(self, other):
-        return math.sqrt((self.x - other.x) ** 2 + (self.y - other.y) ** 2)
+        return ((self.x - other.x) ** 2 + (self.y - other.y) ** 2) ** 0.5
     def Perimetr(self, other1, other2):
         return self.DistanceTo(other1) + other1.DistanceTo(other2) + other2.DistanceTo(self)
     def Square(self, Other1, Other2):
-        half_of_perimetr = self.Perimetr(Other1, Other2)
+        p = self.Perimetr(Other1, Other2) / 2
         a = self.DistanceTo(Other1)
         b = Other1.DistanceTo(Other2)
         c = Other2.DistanceTo(self)
-        return math.sqrt(half_of_perimetr * (half_of_perimetr - a) * (half_of_perimetr - b) * (half_of_perimetr - c))
+        return (p * (p - a) * (p - b) * (p - c)) ** 0.5
 
 N = int(input())
 Points = []
 for i in range(N):
     Points.append(Point(input()))
 Max_square = Points[0].Square(Points[1], Points[2])
+A = Points[0]
+B = Points[1]
+C = Points[2]
 for point1 in Points:
       for point2 in Points:
           for point3 in Points:
               if point1.Square(point2, point3) > Max_square:
                   Max_square = point1.Square(point2, point3)
-print(Max_square)
+                  A = point1
+                  B = point2
+                  C = point3
+print(A, B, C, Max_square)
