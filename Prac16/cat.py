b7b2c26709c4bb435845e1d9a669de00b67c9e21
I'm sexy and I know it <mukhamedzarifovakf@gmail.com>
2016-05-16 16:23:25 +0300

    cat.py uploaded

---
+++ Prac16/cat.py
@@ -0,0 +1,10 @@
+import sys
+import os
+
+if len(sys.argv) >= 2:
+  for file in sys.argv[1:]:
+	  	file = open(sys.argv[i], 'r')
+		  print(''.join(filel.readlines())
+else:
+	print('At least 1 file must be specified')
+	exit(-1)
