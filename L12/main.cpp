8e23a87a2942ab67da5276de5f6763d37c4b7ab7
egorrozinskiy <egor.rozinskiy@mail.ru>
2016-11-24 18:17:08 +0300

    Ex_6 added

--- L12/main.cpp
+++ L12/main.cpp
@@ -1,5 +1,7 @@
 #include<iostream>
 
+using namespace std;
+
 template<int K, int L, int M, int N, int P, int R, int S>
 class DimQ
 {
@@ -10,21 +12,41 @@ public:
     {
         value = _value;
     };
+    double get_value()
+    {
+        return this->value;
+    }
+    //перегрузка операторов
     DimQ<K, L, M, N, P, R, S>  operator-()
     {
         value = -value;
     };
+
     DimQ<K, L, M, N, P, R, S> operator= (const DimQ<K, L, M, N, P, R, S> & other)
     {
         value = other.value;
     };
+
     DimQ<K, L, M, N, P, R, S> operator+ (const DimQ<K, L, M, N, P, R, S> & other)
     {
         value = value + other.value;
     };
+
     DimQ<K, L, M, N, P, R, S> operator- (const DimQ<K, L, M, N, P, R, S> & other)
     {
         value = value - other.value;
+    };
+
+    template<int k, int l, int m, int n, int p, int r, int s>
+    DimQ<K+k, L+l, M+m, N+n, P+p, R+r, S+s> operator* (DimQ<k, l, m, n, p, r, s> &other)
+    {
+        return DimQ<K+k, L+l, M+m, N+n, P+p, R+r, S+s>(this->value * other.get_value());
+    };
+
+    template<int k, int l, int m, int n, int p, int r, int s>
+    DimQ<K-k, L-l, M-m, N-n, P-p, R-r, S-s> operator/ (DimQ<k, l, m, n, p, r, s> &other)
+    {
+        return DimQ<K-k, L-l, M-m, N-n, P-p, R-r, S-s>(this->value / other.get_value());
     };
 };
 
@@ -41,5 +63,24 @@ typedef DimQ<0, 0, 0, 0, 0, 0, 1> Light;
 
 int main()
 {
+    // Длина
+    Length l = {100};
+    // Время
+    Time t = {20};
 
+    // Скорость
+    Velocity v = l / t;
+
+    // Ускорение
+    Acceleration a = v / t;
+
+    // Размерная величина
+    auto smth = v*a*a/t;
+    // Безразмерная величина
+    auto dimensionless = v/v;
+
+// Ошибка компиляции!
+    Dimensionless d = v;
+// Mass m = v;
+    Amount a = d;
 }
